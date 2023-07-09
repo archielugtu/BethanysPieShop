e274a89574849795a0fbdb401e0bf1a03cc80631
Rchi Lugtu <archeeeese.dev@gmail.com>
2023-07-10 09:41:21 +1200

    Added new search page for pies
    uses jquery and ajax to post request to the search endpoint

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -4,57 +4,61 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
 {
-    public class PieController : Controller
-    {
-        private readonly IPieRepository _pieRepository;
-        private readonly ICategoryRepository _categoryRepository;
+			public class PieController : Controller
+			{
+					private readonly IPieRepository _pieRepository;
+					private readonly ICategoryRepository _categoryRepository;
 
-        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
-        {
-            // This is an example of constructor injection since we have already registered both of these reposities in Program.cs at the start of the program.
-            // Therefore the instance of these reposities will get injected into the constructor (automatically) and we can use these values to set the private fields in this controller.
-            _pieRepository = pieRepository;
-            _categoryRepository = categoryRepository;
-        }
+					public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+				{
+					// This is an example of constructor injection since we have already registered both of these reposities in Program.cs at the start of the program.
+					// Therefore the instance of these reposities will get injected into the constructor (automatically) and we can use these values to set the private fields in this controller.
+					_pieRepository = pieRepository;
+					_categoryRepository = categoryRepository;
+				}
 
-        /*
-         * IActionResult is the overarching interface for all different types of results implement
-         */
-       /* public IActionResult List()
-        {
-            PieListViewModel pieListViewModel = new(_pieRepository.AllPies, "All Yummy Pies");
-            return View(pieListViewModel);
-        }*/
+						/*
+						 * IActionResult is the overarching interface for all different types of results implement
+						 */
+						/* public IActionResult List()
+							PieListViewModel pieListViewModel = new(_pieRepository.AllPies, "All Yummy Pies");
+							return View(pieListViewModel);
+						}*/
 
-        public ViewResult List(string category)
-        {
-            IEnumerable<Pie> pies;
-            string? currentCategory;
+						public ViewResult List(string category)
+						{
+							IEnumerable<Pie> pies;
+							string? currentCategory;
 
-            if (string.IsNullOrEmpty(category))
-            {
-                pies = _pieRepository.AllPies.OrderBy(p => p.Name);
-                currentCategory = "All pies";
-            }
-            else
-            {
-                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
-                    .OrderBy(p => p.PieId);
-                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
-            }
+							if (string.IsNullOrEmpty(category))
+							{
+								pies = _pieRepository.AllPies.OrderBy(p => p.Name);
+								currentCategory = "All pies";
+							}
+							else
+							{
+								pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+									.OrderBy(p => p.PieId);
+								currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+							}
 
-            return View(new PieListViewModel(pies, currentCategory));
-
+							return View(new PieListViewModel(pies, currentCategory));
 
 
-        }
 
-        public IActionResult Details(int id)
-        {
-            var pie = _pieRepository.GetPieById(id);
-            if (pie == null)
-                return NotFound();
-            return View(pie);
-        }
-    }
+						}
+
+						public IActionResult Details(int id)
+						{
+							var pie = _pieRepository.GetPieById(id);
+							if (pie == null)
+								return NotFound();
+							return View(pie);
+						}
+
+						public IActionResult Search()
+						{
+							return View();
+						}
+				}
 }
